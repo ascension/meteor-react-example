8afe2d809ff3a6ed1804fb9d5ea247af7408b8cd
Jerrod Bunce <jerrod@gameplanlabs.com>
2015-12-05 21:41:54 -0700

    Change Oauth loginstyle

--- server/social-config.js
+++ server/social-config.js
@@ -5,5 +5,6 @@ ServiceConfiguration.configurations.remove({
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
     appId: '413203205539673',
-    secret: '1033264270481ffcfdb2d1e914511ae4'
+    secret: '1033264270481ffcfdb2d1e914511ae4',
+    loginStyle: 'redirect'
 });
